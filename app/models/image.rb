2d59964da075329b1eaf9e33d69193484a2a5cfe
Kurt Siegfried <kurt.siegfried@gmail.com>
2012-05-13 23:20:32 -0500

    Add validator, trendy *fork me* logo.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :description, :title, :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_presence_of :avatar, :title
 end
